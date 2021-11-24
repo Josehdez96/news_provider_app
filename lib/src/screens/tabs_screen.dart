9b0e55161f2cbb1ec72321b8f1e7c0b027f8f46c
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-11-23 19:37:28 -0500

    feat(tabs_screen): initial screens and bottom nav bar

--- lib/src/screens/tabs_screen.dart
+++ lib/src/screens/tabs_screen.dart
@@ -5,18 +5,48 @@ class TabsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.blue,
-          )
-        ],
-      )
+    return const Scaffold(
+      body: _Screens(),
+      bottomNavigationBar: _NavigationBar(),
+    );
+  }
+}
+
+class _NavigationBar extends StatelessWidget {
+  const _NavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'For you'),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Headers'),
+      ]
+    );
+  }
+}
+
+class _Screens extends StatelessWidget {
+  const _Screens({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // physics: const BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        )
+      ],
     );
   }
 }
