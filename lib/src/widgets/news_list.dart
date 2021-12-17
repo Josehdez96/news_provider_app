bbf14ad95e5ab2e51dfb64bba449af0f6eb6649f
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-12-16 19:17:33 -0500

    feat(news_list): putting card together

--- lib/src/widgets/news_list.dart
+++ lib/src/widgets/news_list.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:news_provider/src/models/news_models.dart';
+import 'package:news_provider/src/widgets/description_card.dart';
+import 'package:news_provider/src/widgets/image_card.dart';
+import 'package:news_provider/src/widgets/title_card.dart';
+import 'package:news_provider/src/widgets/top_bar_card.dart';
 
 class NewsList extends StatelessWidget {
   final List<Article> news;
+
   const NewsList({
     Key? key,
     required this.news,
@@ -13,13 +18,32 @@ class NewsList extends StatelessWidget {
     return ListView.builder(
       itemCount: news.length,
       itemBuilder: (context, index) {
-        return Text(
-          news[index].title,
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-        );
+        return _Article(article: news[index], index: index);
       },
+    );
+  }
+}
+
+class _Article extends StatelessWidget {
+  final Article article;
+  final int index;
+
+  const _Article({
+    required this.article,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 15),
+        TopBarCard(article: article, index: index),
+        TitleCard(article: article, index: index),
+        ImageCard(imageUrl: article.urlToImage),
+        DescriptionCard(description: article.description),
+        const Divider(height: 20)
+      ],
     );
   }
 }
