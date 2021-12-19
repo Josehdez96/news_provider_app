e5fc7e8b007ac17e92651835efa2e6b2a81225a8
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-12-18 20:38:43 -0500

    feat(tab1_screen): maintain scroll state

--- lib/src/screens/tab1_screen.dart
+++ lib/src/screens/tab1_screen.dart
@@ -3,15 +3,25 @@ import 'package:news_provider/src/providers/news_provider.dart';
 import 'package:news_provider/src/widgets/news_list.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Screen extends StatelessWidget {
+class Tab1Screen extends StatefulWidget {
   const Tab1Screen({Key? key}) : super(key: key);
 
   @override
+  State<Tab1Screen> createState() => _Tab1ScreenState();
+}
+
+class _Tab1ScreenState extends State<Tab1Screen>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     final newsProvider = Provider.of<NewsProvider>(context);
 
     return (newsProvider.headlinesStatus == HeadlinesStatus.loading)
         ? const Center(child: CircularProgressIndicator())
         : NewsList(news: newsProvider.headlines);
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
